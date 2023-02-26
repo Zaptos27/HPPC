afee24fcc0ca360b87cc7cbccaa9461c8b743de4
Mikkel <MikkelMM99@gmail.com>
2023-02-26 13:07:10 +0100

    week 3 workering

--- week3/.ipynb_checkpoints/mpi_v2-checkpoint.cpp
+++ week3/.ipynb_checkpoints/mpi_v2-checkpoint.cpp
@@ -35,21 +35,15 @@ int main(int argc, char *argv[])
 
 void master(int workers)
 {
-  int i, tag, who;
-    MPI_Status status;
-    std::array<int, NTASKS> temp, task, result;
-
-    // set up a random number generator
-    std::random_device rd;
-    //std::default_random_engine engine(rd());
-    std::default_random_engine engine;
-    engine.seed(RANDOM_SEED);
-    // make a distribution of random integers in the interval [0:30]
-    std::uniform_int_distribution<int> distribution(0, 30);
-
-    for (int& t : task) {
-        t = distribution(engine);   // set up some "tasks"
-    }
+  
+  int i, result[MAX_TASKS], temp[MAX_TASKS], tag, who;
+  MPI_Status status;
+  int task[MAX_TASKS];// = {5, 5, 5, 2, 5, 2, 2, 2, 3, 5, 2, 1, 2, 1, 1, 2};
+  
+  for (int i = 0; i < MAX_TASKS; i++)
+  {
+    task[i] = rand() % 5 + 1;
+  }
   
   MPI_Request request[workers], recived_request[workers];
   for (i = 0; i < workers; i++)
@@ -67,10 +61,14 @@ void master(int workers)
   {
     MPI_Waitany(workers , recived_request , &index , &status);
     MPI_Irecv(&temp[i], 1, MPI_INT, index+1, i, MPI_COMM_WORLD, &recived_request[index]);
+    printf("tag %d, temp[index] %d, result %d \n",status.MPI_TAG, temp[index],result[status.MPI_TAG]);
     MPI_Wait(&request[index] , &status); // confusion
+    printf("tag: %d, source: %d \n", status.MPI_TAG,status.MPI_SOURCE);
     MPI_Isend(&task[i], 1, MPI_INT, index+1, i, MPI_COMM_WORLD, &request[index]);
+    printf("time %d , who %d|| \n", task[i],index+1);
     i++;
   }
+  printf("Wait\n");
   MPI_Waitall(workers, request , MPI_STATUS_IGNORE);
   MPI_Waitall(workers, recived_request , MPI_STATUS_IGNORE);
   for (i = 0; i < workers; i++)
@@ -78,33 +76,61 @@ void master(int workers)
     MPI_Isend(&task[i], 1, MPI_INT, i+1, NO_MORE_TASKS, MPI_COMM_WORLD, &request[i]);
   }
   MPI_Waitall(workers, request , MPI_STATUS_IGNORE);
-  for (int worker=1; worker<=workers; worker++) {
-        int tasksdone = 0; int workdone = 0;
-        for (int itask=0; itask<NTASKS; itask++)
-        if (result[itask]==worker) {
-            tasksdone++;
-            workdone += task[itask];
-        }
-        printf("Master: Worker %d , solved %d tasks\n", worker, tasksdone)
-    }
   
+  for (i=0; i<MAX_TASKS;i++){
+    printf("%d, ",temp[i]);
+  }
+  printf(",,\n");
+  
+  for (i=0; i<MAX_TASKS;i++){
+    printf("%d, ",task[i]);
+  }
+  printf(",,\n");
 }
 
 
+/*
+void worker (int rank)
+{
+   int tasksdone = 0;
+   int workdone  = 0;
+   int task, result, tag;
+   MPI_Status status;
+
+   MPI_Recv(&task, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD,
+                                                      &status);
+   tag = status.MPI_TAG;
+   while (tag != NO_MORE_TASKS) {
+     sleep(task);
+     workdone+=task;
+     tasksdone++;
+     MPI_Send(&rank, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+     MPI_Recv(&task, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD,
+                                                        &status);
+     tag = status.MPI_TAG;
+   }
+   printf("Worker %d did %d tasks totalling a wait of %d seconds \n", rank, tasksdone, workdone);
+}*/
 
 void worker(int rank)
 {
+  int tasksdone = 0;
+  int workdone = 0;
   int task, result, tag;
   int recived;
   MPI_Request request[2];
   MPI_Status status;
   bool first=true;
+  printf("%d", rank);
   MPI_Recv(&task, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
   tag = status.MPI_TAG;
   while (tag != NO_MORE_TASKS)
   {
     MPI_Irecv(&recived , 1 , MPI_INT , 0 , MPI_ANY_TAG , MPI_COMM_WORLD , &request[1]);
-    std::this_thread::sleep_for(std::chrono::milliseconds(task));
+    sleep(task);
+    result = rank;
+    workdone += task;
+    tasksdone++;
     
     if(first!=true){
       MPI_Wait(&request[0] , MPI_STATUS_IGNORE);
@@ -112,7 +138,7 @@ void worker(int rank)
     
     first=false;
     
-    MPI_Isend(&rank, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &request[0]); 
+    MPI_Isend(&result, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &request[0]); 
     MPI_Wait(&request[1] , &status);
     
     
@@ -121,4 +147,5 @@ void worker(int rank)
     tag = status.MPI_TAG;
   }
   MPI_Wait(&request[0] , MPI_STATUS_IGNORE); 
+  printf("Worker %d solved %d tasks totalling %d units of work \n", rank, tasksdone, workdone);
 }
