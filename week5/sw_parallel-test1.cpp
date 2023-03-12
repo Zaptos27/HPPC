9e1f4ec0b6eb194e45216f9d396bfc9e452d472c
Mikkel <MikkelMM99@gmail.com>
2023-03-12 12:38:50 +0100

    code for testing

--- week5/sw_parallel-test1.cpp
+++ week5/sw_parallel-test1.cpp
@@ -14,7 +14,7 @@ using grid_t = std::array<std::array<real_t, NX>, NY>;
 
 class Sim_Configuration {
 public:
-    int iter = 1000;  // Number of iterations
+    int iter = 100;  // Number of iterations
     double dt = 0.05;       // Size of the integration time step
     real_t g = 9.80665;     // Gravitational acceleration
     real_t dx = 1;          // Integration step size in the horizontal direction
@@ -66,11 +66,12 @@ public:
     grid_t v{}; // The speed in the vertical direction.
     grid_t e{}; // The water elevation.
     Water() {
-        for (size_t i = 1; i < NY - 1; ++i) 
-        for (size_t j = 1; j < NX - 1; ++j) {
-            real_t ii = 100.0 * (i - (NY - 2.0) / 2.0) / NY;
-            real_t jj = 100.0 * (j - (NX - 2.0) / 2.0) / NX;
-            e[i][j] = std::exp(-0.02 * (ii * ii + jj * jj));
+        for (size_t i = 1; i < NY - 1; ++i) {
+            for (size_t j = 1; j < NX - 1; ++j) {
+                real_t ii = 100.0 * (i - (NY - 2.0) / 2.0) / NY;
+                real_t jj = 100.0 * (j - (NX - 2.0) / 2.0) / NX;
+                e[i][j] = std::exp(-0.02 * (ii * ii + jj * jj));
+            }
         }
     }
 };
@@ -90,15 +91,12 @@ void to_file(const std::vector<grid_t> &water_history, const std::string &filena
  * @param data   The data update, which could be the water elevation `e` or the speed in the horizontal direction `u`.
  * @param shape  The shape of data including the ghost lines.
  */
+
 void exchange_horizontal_ghost_lines(grid_t& data) {
-    #pragma data copy(data)
-    {//pragma
-    #pragma acc loop
     for (uint64_t j = 0; j < NX; ++j) {
         data[0][j]      = data[NY-2][j]; 
         data[NY-1][j]   = data[1][j];
     }
-    }//pragma
 }
 
 /** Exchange the vertical ghost lines i.e. copy the second data column to the rightmost data column and vice versa.
@@ -106,43 +104,39 @@ void exchange_horizontal_ghost_lines(grid_t& data) {
  * @param data   The data update, which could be the water elevation `e` or the speed in the vertical direction `v`.
  * @param shape  The shape of data including the ghost lines.
  */
+
 void exchange_vertical_ghost_lines(grid_t& data) {
-    #pragma data copy(data)
-    {//pragma
-    #pragma acc loop
     for (uint64_t i = 0; i < NY; ++i) {
         data[i][0] = data[i][NX-2];
         data[i][NX-1] = data[i][1];
     }
-    }//pragma
 }
 
 /** One integration step
  *
  * @param w The water world to update.
  */
-#pragma routine seq
+
 void integrate(Water &w, const real_t dt, const real_t dx, const real_t dy, const real_t g) {
     exchange_horizontal_ghost_lines(w.e);
-    exchange_horizontal_ghost_lines(w.v);
+    exchange_horizontal_ghost_lines(w.v); 
     exchange_vertical_ghost_lines(w.e);
     exchange_vertical_ghost_lines(w.u);
-
-    #pragma acc parallel loop //test gang vector and  gang collapse
-    for (uint64_t i = 0; i < NY - 1; ++i) 
-    #pragma acc parallel loop
-    for (uint64_t j = 0; j < NX - 1; ++j) {
-        w.u[i][j] -= dt / dx * g * (w.e[i][j+1] - w.e[i][j]);
-        w.v[i][j] -= dt / dy * g * (w.e[i + 1][j] - w.e[i][j]);
+    
+    #pragma acc parallel loop collapse(2) present(w.e, w.u, w.v)
+    for (uint64_t i = 0; i < NY - 1; ++i) {
+        for (uint64_t j = 0; j < NX - 1; ++j) {
+            w.u[i][j] -= dt / dx * g * (w.e[i][j+1] - w.e[i][j]);
+            w.v[i][j] -= dt / dy * g * (w.e[i + 1][j] - w.e[i][j]);
+        }
     }
-    #pragma acc parallel loop
-    for (uint64_t i = 1; i < NY - 1; ++i) 
-    #pragma acc parallel loop
-    for (uint64_t j = 1; j < NX - 1; ++j) {
-        w.e[i][j] -= dt / dx * (w.u[i][j] - w.u[i][j-1])
-                   + dt / dy * (w.v[i][j] - w.v[i-1][j]);
+    #pragma acc parallel loop collapse(2) present(w.u, w.v, w.e)
+    for (uint64_t i = 1; i < NY - 1; ++i) {
+        for (uint64_t j = 1; j < NX - 1; ++j) {
+            w.e[i][j] -= dt / dx * (w.u[i][j] - w.u[i][j-1])
+                          + dt / dy * (w.v[i][j] - w.v[i-1][j]);
+        }
     }
-
 }
 
 /** Simulation of shallow water
@@ -156,15 +150,17 @@ void simulate(const Sim_Configuration config) {
 
     std::vector <grid_t> water_history;
     auto begin = std::chrono::steady_clock::now();
-    #pragma acc data copy(water_world, water_history) //mabye create
+    
+    //integrate called config.iter times
+
+    #pragma acc data copy(water_world)
     {
-    #pragma acc serial
-    for (uint64_t t = 0; t < config.iter; ++t) {
-        integrate(water_world, config.dt,  config.dx, config.dy, config.g);
-        if (t % config.data_period == 0) {
-            water_history.push_back(water_world.e);
+        for (uint64_t t = 0; t < config.iter; ++t) { 
+            integrate(water_world, config.dt,  config.dx, config.dy, config.g);
+            if (t % config.data_period == 0) {
+                water_history.push_back(water_world.e);
+            }
         }
-    }
     }
     auto end = std::chrono::steady_clock::now();
 
@@ -177,6 +173,14 @@ void simulate(const Sim_Configuration config) {
 /** Main function that parses the command line and start the simulation */
 int main(int argc, char **argv) {
     auto config = Sim_Configuration({argv, argv+argc});
+    
+    std::chrono::time_point<std::chrono::system_clock> tstart2, tend2;
+    tstart2 = std::chrono::high_resolution_clock::now();
+    
     simulate(config);
+    
+    tend2 = std::chrono::high_resolution_clock::now();
+    std::cout << "This is time when using simulate " << (tend2 - tstart2).count()*1e-9 << "\n";
+    
     return 0;
 }
